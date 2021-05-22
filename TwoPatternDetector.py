060323ea8094e7325ddf4c29dc1c73bb313e9367
GianlucaSpinelli <gianluca.spinelli97@hotmail.it>
2021-05-22 11:06:03 +0200

    aggiunti pattern continuazione a 2

--- TwoPatternDetector.py
+++ TwoPatternDetector.py
@@ -64,8 +64,28 @@ class TwoPatternDetector:
                     vector[4].P.append(49)  # One Red Crow
                     return
 
+                if ((vector[0].EMAvalue >= vector[1].EMAvalue >= vector[2].EMAvalue >= vector[3].EMAvalue) and vector[3].candleType == 1
+                        and vector[3].open > vector[3].close
+                        and vector[4].open < vector[4].close
+                        and vector[4].open <= vector[3].minC
+                        and abs(vector[3].close - vector[4].open) > 2 * abs(vector[3].close - vector[3].minC)
+                        and abs(vector[4].close - vector[3].close) > 2 * abs(vector[3].close - vector[3].minC)
+                        and vector[4].boc < vector[3].boc): #far controllare
+                    vector[4].P.append[96]  # Bearish Thrusting lines
+                    return
 
-            if (vector[4].CBR >=2):
+                if ((vector[0].EMAvalue <= vector[1].EMAvalue <= vector[2].EMAvalue <= vector[3].EMAvalue) and vector[3].candleType == 1
+                        and vector[3].open < vector[3].close
+                        and vector[4].open > vector[4].close
+                        and vector[4].open >= vector[3].maxC
+                        and abs(vector[4].close - vector[4].open) > 2 * abs(vector[3].close - vector[3].maxC)
+                        and abs(vector[4].close - vector[3].close) > 2 * abs(vector[3].close - vector[3].maxC)
+                        and vector[4].boc < vector[3].boc): #far controllare
+                    vector[4].P.append[97]  # Bullish Thrusting lines
+                    return
+
+
+            if (type == 2):
 
                 if ((vector[0].EMAvalue >= vector[1].EMAvalue >= vector[2].EMAvalue >= vector[3].EMAvalue >= vector[4].EMAvalue)
                         and vector[3].candleType == 1 and vector[3].open > vector[3].close and vector[4].open > vector[4].close
@@ -89,13 +109,69 @@ class TwoPatternDetector:
                     vector[4].P.append[44]  # Matching Low
                     return
 
-                if ((vector[0].EMAvalue >= vector[1].EMAvalue >= vector[2].EMAvalue >= vector[3].EMAvalue >= vector[4].EMAvalue)
+                if ((vector[0].EMAvalue <= vector[1].EMAvalue <= vector[2].EMAvalue <= vector[3].EMAvalue <= vector[4].EMAvalue)
                         and vector[3].candleType == 1 and vector[3].open < vector[3].close and vector[4].open < vector[4].close
                         and vector[3].open < vector[4].open < vector[3].close and abs(vector[3].close - vector[4].close) <= 0.1 * vector[4].doc
                         and vector[3].doc > vector[4].doc and abs(vector[3].close - vector[3].maxC) <= 0.15 * vector[3].dmM
                         and abs(vector[4].close - vector[4].maxC) <= 0.15 * vector[4].dmM):
                     # opzionali M(t) < o(t-1) e m(t) > c(t-1)
                     vector[4].P.append[45]  # Matching High
+                    return
+
+            if (vector[4].CBR >=2):
+
+                if ((vector[0].EMAvalue <= vector[1].EMAvalue <= vector[2].EMAvalue) and vector[3].CBR >= 2 and vector[3].open > vector[3].close
+                        and vector[4].open < vector[4].close
+                        and abs(vector[3].open - vector[4].open) <= 0.1 * ((vector[3].doc + vector[4].doc)/2)
+                        and vector[4].boc > vector[3].boc):
+                    vector[4].P.append[90]  # Bullish Separating lines
+                    return
+
+                if ((vector[0].EMAvalue >= vector[1].EMAvalue >= vector[2].EMAvalue) and vector[3].CBR >= 2 and vector[3].open < vector[3].close
+                        and vector[4].open > vector[4].close
+                        and abs(vector[3].open - vector[4].open) <= 0.1 * ((vector[3].doc + vector[4].doc)/2)
+                        and vector[4].boc < vector[3].boc):
+                    vector[4].P.append[91]  # Bearish Separating lines
+                    return
+
+            if (type == 2):
+
+                if ((vector[0].EMAvalue >= vector[1].EMAvalue >= vector[2].EMAvalue >= vector[3].EMAvalue) and vector[3].candleType == 1
+                        and vector[3].open > vector[3].close
+                        and vector[4].open < vector[4].close
+                        and vector[4].open <= vector[3].close
+                        and abs(vector[4].close - vector[3].minC) <= 0.1 * ((vector[3].doc + vector[4].doc)/2)
+                        and vector[4].boc < vector[3].boc):
+                    vector[4].P.append[92]  # Bearish One Neck lines
+                    return
+
+                if ((vector[0].EMAvalue <= vector[1].EMAvalue <= vector[2].EMAvalue <= vector[3].EMAvalue) and vector[3].candleType == 1
+                        and vector[3].open < vector[3].close
+                        and vector[4].open > vector[4].close
+                        and vector[4].open > vector[3].close
+                        and abs(vector[4].close - vector[3].maxC) <= 0.1 * ((vector[3].doc + vector[4].doc)/2)
+                        and vector[4].boc > vector[3].boc):
+                    vector[4].P.append[93]  # Bullish One Neck lines
+                    return
+
+                if ((vector[0].EMAvalue >= vector[1].EMAvalue >= vector[2].EMAvalue >= vector[3].EMAvalue) and vector[3].candleType == 1
+                        and vector[3].open > vector[3].close
+                        and vector[4].open < vector[4].close
+                        and vector[4].open <= vector[3].close
+                        and vector[4].close >= vector[3].close
+                        and vector[4].dmM < vector[3].dmM
+                        and vector[4].boc < vector[3].boc):
+                    vector[4].P.append[94]  # Bearish In Neck lines
+                    return
+
+                if ((vector[0].EMAvalue <= vector[1].EMAvalue <= vector[2].EMAvalue <= vector[3].EMAvalue) and vector[3].candleType == 1
+                        and vector[3].open < vector[3].close
+                        and vector[4].open > vector[4].close
+                        and vector[4].open >= vector[3].close
+                        and vector[4].close <= vector[3].close
+                        and vector[4].dmM < vector[3].dmM
+                        and vector[4].boc > vector[3].boc):
+                    vector[4].P.append[94]  # Bullish In Neck lines
                     return
 
             if 2 <= vector[4].CBR <= 3:
