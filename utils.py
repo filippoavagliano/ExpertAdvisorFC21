0eb88d8e3953b193a3c75d0a7e38f57995351523
L043N70 <L043N70@users.noreply.github.com>
2021-05-24 16:07:51 +0200

    Add Bollinger Bands

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from typing import List
 from Candle import Candle
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def get_ema(s: List[Candle], n) -> List:
     """
@@ -33,3 +36,41 @@ def get_ema(s: List[Candle], n) -> List:
         ema.append(tmp)
 
     return ema
+
+
+def get_bollinger_band(bars: pd.DataFrame, period: int):
+    """
+    Calculate the 20 Day Simple Moving Average, Upper Band and Lower Band
+    :param bars: barre ottenute da MetaTrader 5
+    :param period: time period in days
+    :return: (sma, lower, upper)
+    """
+    # Calculating the Simple Moving Average
+    bars['SMA'] = bars['close'].rolling(window=period).mean()
+
+    # Get the standard deviation
+    bars['STD'] = bars['close'].rolling(window=period).std()
+
+    # Calculate the Upper Bollinger Band
+    bars['Upper'] = bars['SMA'] + (bars['STD'] * 2)
+
+    # Calculate the Lower Bollinger Band
+    bars['Lower'] = bars['SMA'] - (bars['STD'] * 2)
+
+    sma = bars['SMA'].to_list()
+    upper = bars['Upper'].to_list()
+    lower = bars['Lower'].to_list()
+
+    return sma, lower, upper
+
+
+def plot_bollinger_band(close, sma, lower, upper):
+    column_list = ['Close', 'SMA', 'Lower', 'Upper']
+    data = list(zip(close, sma, lower, upper))
+
+    df = pd.DataFrame(data, columns=column_list)
+
+    df[column_list].plot(figsize=(12.2, 6.4))  # Plot the data
+    plt.title('Bollinger Band')
+    plt.ylabel('USD Price ($)')
+    plt.show()
